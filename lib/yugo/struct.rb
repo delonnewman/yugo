4b092c67c5644b88fbe941de67023139313d8f53
Delon Newman <delnewman@salud.unm.edu>
2019-11-05 16:24:11 -0700

    added assignment to Yugo::Struct

--- lib/yugo/struct.rb
+++ lib/yugo/struct.rb
@@ -1,4 +1,14 @@
 module Yugo
-  class Struct
+  class Struct < Hash
+    def method_missing(method, value = nil)
+      meth = method.to_s
+      if meth.end_with?('=')
+        name = meth.chop.to_sym
+        self[name] = value
+      else
+        raise ArgumentError, "#{method} expected 0 arguments got 1" unless value.nil?
+        self[method]
+      end
+    end
   end
 end
